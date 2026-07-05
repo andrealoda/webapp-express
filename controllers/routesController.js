123f93a67d689019ac889f4c7e3abc434711912e
andrea8980 <andrea@casaloda.it>
2026-07-05 17:52:34 +0200

    refactor: move movie route handlers to routesController for better organization

---
+++ controllers/routesController.js
@@ -0,0 +1,32 @@
+const connection = require('../data/db_connection');
+
+const routesController = {
+    // INDEX ROUTE
+
+    getAllMovies: (req, res) => {
+        const query = 'SELECT * FROM movies';
+        connection.query(query, (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).json({ error: 'Errore nel recupero delle informazioni sui film' });
+            }
+            res.json(results);
+        });
+    },
+
+    // SHOW ROUUTE
+    getMovieById: (req, res) => {
+        const movieId = req.params.id;
+        const query = 'SELECT * FROM movies WHERE id = ?';
+        connection.query(query, [movieId], (err, results) => {
+            if (err) return res.status(500).json({ error: 'Errore nel recupero delle informazioni sul film scelto' });
+            if (results.length === 0) return res.status(404).json({ error: 'Film non trovato' });
+            res.json(results);
+        });
+    }
+
+
+}
+
+
+module.exports = routesController;
--- routers/routes.js
+++ routers/routes.js
@@ -1,28 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const connection = require('../data/db_connection');
+const routesController = require('../controllers/routesController');
+
 
 //INDEX ROUTE
-router.get('/movies', (req, res) => {
-    const query = 'SELECT * FROM movies';
-    connection.query(query, (err, results) => {
-        if (err) {
-            console.log(err);
-            return res.status(500).json({ error: 'Errore nel recupero delle informazioni sui film' });
-        }
-        res.json(results);
-    });
-});
+router.get('/movies', routesController.getAllMovies);
 
 //SHOW ROUTE
-router.get('/movies/:id', (req, res) => {
-    const movieId = req.params.id;
-    const query = 'SELECT * FROM movies WHERE id = ?';
-    connection.query(query, [movieId], (err, results) => {
-        if (err) return res.status(500).json({ error: 'Errore nel recupero delle informazioni sul film scelto' });
-        if (results.length === 0) return res.status(404).json({ error: 'Film non trovato' });
-        res.json(results);
-    });
-});
+router.get('/movies/:id', routesController.getMovieById);
 
 module.exports = router;
