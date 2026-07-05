4b2b2a2bc7714af396168e13f923587599b09adc
andrea8980 <andrea@casaloda.it>
2026-07-05 17:18:40 +0200

    refactor: update server to use dynamic port with fallback; add database connection file

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.API_SERVER_PORT;
+const port = process.env.API_SERVER_PORT || 3000;
 
 
 app.use(express.static('public'));
